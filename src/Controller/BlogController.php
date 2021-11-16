1851016b967f9617918a34f1405afef9e31a8a6a
yosrimhamdi <yosrimhamdi@outlook.com>
2021-11-16 22:30:43 +0100

    Dealing with form .

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController {
   /**
@@ -19,7 +22,22 @@ class BlogController extends AbstractController {
   /**
    * @Route("/blog/new", name="blog_new")
    */
-  public function create(): Response {
+  public function create(
+    Request $request,
+    EntityManagerInterface $manager
+  ): Response {
+    if ($request->request->count() > 0) {
+      $article = new Article();
+      $article
+        ->setName($request->request->get('name'))
+        ->setDescription($request->request->get('description'))
+        ->setImageURL($request->request->get('imageURL'))
+        ->setCreatedAt(new \DateTime());
+
+      $manager->persist($article);
+      $manager->flush();
+    }
+
     return $this->render('blog/create.html.twig');
   }
 }
