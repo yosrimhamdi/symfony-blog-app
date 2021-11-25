3d7e4adf4b6d8d49527b98444ed24225d62bc234
yosrimhamdi <yosrimhamdi@outlook.com>
2021-11-25 11:32:21 +0100

    Using form_row instead of form_widget.

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,89 +4,81 @@ namespace App\Entity;
 
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
  */
-class Article
-{
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+class Article {
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @ORM\Column(type="integer")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
+  /**
+   * @ORM\Column(type="string", length=255)
+   * @Assert\Length(min=10, max=255)
+   */
+  private $name;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $imageURL;
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $imageURL;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $description;
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $description;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
+  /**
+   * @ORM\Column(type="datetime")
+   */
+  private $createdAt;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int {
+    return $this->id;
+  }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
+  public function getName(): ?string {
+    return $this->name;
+  }
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
+  public function setName(string $name): self {
+    $this->name = $name;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getImageURL(): ?string
-    {
-        return $this->imageURL;
-    }
+  public function getImageURL(): ?string {
+    return $this->imageURL;
+  }
 
-    public function setImageURL(string $imageURL): self
-    {
-        $this->imageURL = $imageURL;
+  public function setImageURL(string $imageURL): self {
+    $this->imageURL = $imageURL;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
+  public function getDescription(): ?string {
+    return $this->description;
+  }
 
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
+  public function setDescription(string $description): self {
+    $this->description = $description;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
+  public function getCreatedAt(): ?\DateTimeInterface {
+    return $this->createdAt;
+  }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
+  public function setCreatedAt(\DateTimeInterface $createdAt): self {
+    $this->createdAt = $createdAt;
 
-        return $this;
-    }
+    return $this;
+  }
 }
