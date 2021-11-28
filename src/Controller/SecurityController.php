db6e1159009be113a3ecf8fd924a6dc06e50e66e
yosrimhamdi <yosrimhamdi@outlook.com>
2021-11-28 12:28:36 +0100

    Changed a controller name.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class AuthController extends AbstractController {
+class SecurityController extends AbstractController {
   /**
    * @Route("/register", name="auth")
    */
