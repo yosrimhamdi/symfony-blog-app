440ea561665e586ad2618b6f1577765ed16f8f9b
yosrimhamdi <yosrimhamdi@outlook.com>
2021-11-28 15:32:46 +0100

    Created the login form.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\LoginType;
 use App\Form\RegisterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,6 +35,17 @@ class SecurityController extends AbstractController {
     }
 
     return $this->render('security/register.html.twig', [
+      'form' => $form->createView(),
+    ]);
+  }
+
+  /**
+   * @Route("/login", name="security_login")
+   */
+  public function login() {
+    $form = $this->createForm(LoginType::class);
+
+    return $this->render('security/login.html.twig', [
       'form' => $form->createView(),
     ]);
   }
